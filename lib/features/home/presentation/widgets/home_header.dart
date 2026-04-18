6d8da20e20ad4a419c96a31a26d8eb6a351cf9ec
KaziAsif406 <157726357+KaziAsif406@users.noreply.github.com>
2026-04-18 18:25:39 +0600

    Home Screen Complete

---
+++ lib/features/home/presentation/widgets/home_header.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template_flutter/common_widgets/custom_button.dart';
+import 'package:template_flutter/common_widgets/custom_textform_field.dart';
+import 'package:template_flutter/constants/text_font_style.dart';
+import 'package:template_flutter/gen/colors.gen.dart';
+import 'package:template_flutter/helpers/all_routes.dart';
+import 'package:template_flutter/helpers/navigation_service.dart';
+import 'package:template_flutter/helpers/ui_helpers.dart';
+// import 'package:template_flutter/helpers/helper_methods.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.allSecondaryColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(16.r),
+          bottomRight: Radius.circular(16.r),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.c14181F.withValues(alpha: 0.06),
+            blurRadius: 10,
+            offset: const Offset(0, 8),
+          ),
+        ],
+      ),
+      // padding: EdgeInsets.fromLTRB(20.w, 5.h, 20.w, 26.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Image.asset(
+                'assets/images/home_header.jpeg',
+                width: double.infinity,
+                height: 200.h,
+                fit: BoxFit.cover,
+              ),
+              Positioned.fill(
+                child: Container(
+                  width: double.infinity,
+                  height: 200.h,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        AppColors.c000000.withValues(alpha: 0.7),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 60.h,
+                left: 20.w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Find a Trusted Service Professional',
+                      style: TextFontStyle.textStyle20cFFFFFFInter700,
+                    ),
+                    UIHelper.verticalSpace(8.h),
+                    Text(
+                      'Get free quotes from top-rated contractors',
+                      style: TextFontStyle.textStyle14cFFFFFFInter400
+                          .copyWith(color: AppColors.scaffoldColor.withValues(alpha: 0.8)),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16.w),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.r),
+            ),
+            child: Column(
+              children: [
+                CustomTextFormField(
+                  hintText: 'What service do you need?',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: AppColors.c6A7181,
+                    size: 20.sp,
+                  )
+                ),
+                UIHelper.verticalSpace(8.h),
+                Row(
+                  children: [
+                    CustomTextFormField(
+                      width: 216.w,
+                      hintText: 'Zip code',
+                    ),
+                    UIHelper.horizontalSpace(12.7.w),
+                    CustomButton(
+                      width: 85.w,
+                      label: 'Search',
+                      onPressed: () {
+                        NavigationService.navigateTo(Routes.findLocationScreen);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/widgets/service_catagories.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template_flutter/constants/text_font_style.dart';
+import 'package:template_flutter/gen/colors.gen.dart';
+import 'package:template_flutter/helpers/ui_helpers.dart';
+
+class ServiceCategoriesSection extends StatelessWidget {
+  const ServiceCategoriesSection({super.key});
+
+  static final List<_CategoryData> _categories = [
+    _CategoryData(label: 'Plumbing', iconPath: 'assets/icons/plumbing.png'),
+    _CategoryData(label: 'HVAC', iconPath: 'assets/icons/hvac.png'),
+    _CategoryData(label: 'Roofing', iconPath: 'assets/icons/roofing.png'),
+    _CategoryData(label: 'Electrical', iconPath: 'assets/icons/electrical.png'),
+    _CategoryData(label: 'Cleaning', iconPath: 'assets/icons/cleaning.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Row(
+            children: [
+              Text(
+                'Service Categories',
+                style: TextFontStyle.textStyle16c0A0A0AInter700,
+              ),
+              const Spacer(),
+              Text(
+                'See All >',
+                style: TextFontStyle.textStyle14cBE1E2DInter500,
+              ),
+            ],
+          ),
+        ),
+        UIHelper.verticalSpace(12.h),
+        SizedBox(
+          height: 100.h,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: _categories.length,
+            separatorBuilder: (_, __) => UIHelper.horizontalSpace(5.w),
+            itemBuilder: (context, index) {
+              return _CategoryTile(category: _categories[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CategoryData {
+  const _CategoryData({required this.label, required this.iconPath});
+
+  final String label;
+  final String iconPath;
+}
+
+class _CategoryTile extends StatelessWidget {
+  const _CategoryTile({required this.category});
+
+  final _CategoryData category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 56.w,
+          height: 56.w,
+          decoration: BoxDecoration(
+            color: AppColors.cE54545.withValues(alpha: 0.12),
+            borderRadius: BorderRadius.circular(18.r),
+          ),
+          child: Center(
+            child: Image.asset(
+              category.iconPath,
+              width: 24.w,
+              height: 24.h,
+            ),
+          ),
+        ),
+        UIHelper.verticalSpace(6.h),
+        SizedBox(
+          width: 80.w,
+          child: Text(
+            category.label,
+            textAlign: TextAlign.center,
+            style: TextFontStyle.textStyle12c0A0A0AInter400,
+          ),
+        ),
+      ],
+    );
+  }
+}
