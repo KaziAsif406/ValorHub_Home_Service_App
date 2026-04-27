363e20e9d7d207ed89f526da81e67161b6c8cd76
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-04-28 05:43:34 +0600

    Path Provider added to save Profile Image

--- lib/helpers/app_preferences.dart
+++ lib/helpers/app_preferences.dart
@@ -1,6 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPrefs {
+  static const String _profileImagePathKey = 'profile_image_path';
+
   static Future<void> setOnboardingSeen() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('onboarding_seen', true);
@@ -19,5 +21,20 @@ class AppPrefs {
   static Future<bool> isLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('logged_in') ?? false;
+  }
+
+  static Future<void> setProfileImagePath(String? value) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (value == null || value.trim().isEmpty) {
+      await prefs.remove(_profileImagePathKey);
+      return;
+    }
+
+    await prefs.setString(_profileImagePathKey, value);
+  }
+
+  static Future<String?> getProfileImagePath() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_profileImagePathKey);
   }
 }
