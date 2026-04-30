109c384121cb91b327722e9668475a7a4fcbe66e
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-05-01 01:37:07 +0600

    Routing updated

--- lib/features/contractor/inbox/presentation/chat_inbox.dart
+++ lib/features/contractor/inbox/presentation/chat_inbox.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:template_flutter/features/contractor/inbox/presentation/widget/chat_bubble.dart';
-import 'package:template_flutter/features/customer/contractors/presentation/widgets/contractor_info.dart';
+import 'package:template_flutter/features/customer/contractors/data/contractor_model.dart';
 import 'package:template_flutter/features/contractor/inbox/presentation/widget/composer.dart';
 import 'package:template_flutter/features/contractor/inbox/presentation/widget/inbox_header.dart';
 
