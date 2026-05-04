f7272e64f08ed0106e6c558357b972ae1203660f
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-05-05 04:36:23 +0600

    FlutterGen Modified

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,454 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/back.png
+  AssetGenImage get back => const AssetGenImage('assets/icons/back.png');
+
+  /// File path: assets/icons/call_red.png
+  AssetGenImage get callRed => const AssetGenImage('assets/icons/call_red.png');
+
+  /// File path: assets/icons/certified.png
+  AssetGenImage get certified =>
+      const AssetGenImage('assets/icons/certified.png');
+
+  /// File path: assets/icons/cleaning.png
+  AssetGenImage get cleaning =>
+      const AssetGenImage('assets/icons/cleaning.png');
+
+  /// File path: assets/icons/clipboard.png
+  AssetGenImage get clipboard =>
+      const AssetGenImage('assets/icons/clipboard.png');
+
+  /// File path: assets/icons/clock.png
+  AssetGenImage get clock => const AssetGenImage('assets/icons/clock.png');
+
+  /// File path: assets/icons/clock_red.png
+  AssetGenImage get clockRed =>
+      const AssetGenImage('assets/icons/clock_red.png');
+
+  /// File path: assets/icons/cross.png
+  AssetGenImage get cross => const AssetGenImage('assets/icons/cross.png');
+
+  /// File path: assets/icons/down_arrow.png
+  AssetGenImage get downArrow =>
+      const AssetGenImage('assets/icons/down_arrow.png');
+
+  /// File path: assets/icons/electrical.png
+  AssetGenImage get electrical =>
+      const AssetGenImage('assets/icons/electrical.png');
+
+  /// File path: assets/icons/filled_heart.png
+  AssetGenImage get filledHeart =>
+      const AssetGenImage('assets/icons/filled_heart.png');
+
+  /// File path: assets/icons/filter.png
+  AssetGenImage get filter => const AssetGenImage('assets/icons/filter.png');
+
+  /// File path: assets/icons/gold_star.png
+  AssetGenImage get goldStar =>
+      const AssetGenImage('assets/icons/gold_star.png');
+
+  /// File path: assets/icons/green_tick.png
+  AssetGenImage get greenTick =>
+      const AssetGenImage('assets/icons/green_tick.png');
+
+  /// File path: assets/icons/hvac.png
+  AssetGenImage get hvac => const AssetGenImage('assets/icons/hvac.png');
+
+  /// File path: assets/icons/location_pin.png
+  AssetGenImage get locationPin =>
+      const AssetGenImage('assets/icons/location_pin.png');
+
+  /// File path: assets/icons/location_pin_red.png
+  AssetGenImage get locationPinRed =>
+      const AssetGenImage('assets/icons/location_pin_red.png');
+
+  /// File path: assets/icons/locator.png
+  AssetGenImage get locator => const AssetGenImage('assets/icons/locator.png');
+
+  /// File path: assets/icons/logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/icons/logo.png');
+
+  /// File path: assets/icons/logo_home.png
+  AssetGenImage get logoHome =>
+      const AssetGenImage('assets/icons/logo_home.png');
+
+  /// File path: assets/icons/mail.png
+  AssetGenImage get mail => const AssetGenImage('assets/icons/mail.png');
+
+  /// File path: assets/icons/mail_red.png
+  AssetGenImage get mailRed => const AssetGenImage('assets/icons/mail_red.png');
+
+  /// File path: assets/icons/nav_document.png
+  AssetGenImage get navDocument =>
+      const AssetGenImage('assets/icons/nav_document.png');
+
+  /// File path: assets/icons/nav_heart.png
+  AssetGenImage get navHeart =>
+      const AssetGenImage('assets/icons/nav_heart.png');
+
+  /// File path: assets/icons/nav_home.png
+  AssetGenImage get navHome => const AssetGenImage('assets/icons/nav_home.png');
+
+  /// File path: assets/icons/nav_profile.png
+  AssetGenImage get navProfile =>
+      const AssetGenImage('assets/icons/nav_profile.png');
+
+  /// File path: assets/icons/notification.png
+  AssetGenImage get notification =>
+      const AssetGenImage('assets/icons/notification.png');
+
+  /// File path: assets/icons/notification_icon.png
+  AssetGenImage get notificationIcon =>
+      const AssetGenImage('assets/icons/notification_icon.png');
+
+  /// File path: assets/icons/phone.png
+  AssetGenImage get phone => const AssetGenImage('assets/icons/phone.png');
+
+  /// File path: assets/icons/plumbing.png
+  AssetGenImage get plumbing =>
+      const AssetGenImage('assets/icons/plumbing.png');
+
+  /// File path: assets/icons/rating.png
+  AssetGenImage get rating => const AssetGenImage('assets/icons/rating.png');
+
+  /// File path: assets/icons/roofing.png
+  AssetGenImage get roofing => const AssetGenImage('assets/icons/roofing.png');
+
+  /// File path: assets/icons/search.png
+  AssetGenImage get search => const AssetGenImage('assets/icons/search.png');
+
+  /// File path: assets/icons/trusted.png
+  AssetGenImage get trusted => const AssetGenImage('assets/icons/trusted.png');
+
+  /// File path: assets/icons/up_arrow.png
+  AssetGenImage get upArrow => const AssetGenImage('assets/icons/up_arrow.png');
+
+  /// File path: assets/icons/upload.png
+  AssetGenImage get upload => const AssetGenImage('assets/icons/upload.png');
+
+  /// File path: assets/icons/verified_green.png
+  AssetGenImage get verifiedGreen =>
+      const AssetGenImage('assets/icons/verified_green.png');
+
+  /// File path: assets/icons/verified_red.png
+  AssetGenImage get verifiedRed =>
+      const AssetGenImage('assets/icons/verified_red.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        back,
+        callRed,
+        certified,
+        cleaning,
+        clipboard,
+        clock,
+        clockRed,
+        cross,
+        downArrow,
+        electrical,
+        filledHeart,
+        filter,
+        goldStar,
+        greenTick,
+        hvac,
+        locationPin,
+        locationPinRed,
+        locator,
+        logo,
+        logoHome,
+        mail,
+        mailRed,
+        navDocument,
+        navHeart,
+        navHome,
+        navProfile,
+        notification,
+        notificationIcon,
+        phone,
+        plumbing,
+        rating,
+        roofing,
+        search,
+        trusted,
+        upArrow,
+        upload,
+        verifiedGreen,
+        verifiedRed
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/error_image.png
+  AssetGenImage get errorImage =>
+      const AssetGenImage('assets/images/error_image.png');
+
+  /// File path: assets/images/home_popular_1.png
+  AssetGenImage get homePopular1 =>
+      const AssetGenImage('assets/images/home_popular_1.png');
+
+  /// File path: assets/images/home_popular_2.png
+  AssetGenImage get homePopular2 =>
+      const AssetGenImage('assets/images/home_popular_2.png');
+
+  /// File path: assets/images/home_popular_3.png
+  AssetGenImage get homePopular3 =>
+      const AssetGenImage('assets/images/home_popular_3.png');
+
+  /// File path: assets/images/home_popular_4.png
+  AssetGenImage get homePopular4 =>
+      const AssetGenImage('assets/images/home_popular_4.png');
+
+  /// File path: assets/images/onboarding_1.png
+  AssetGenImage get onboarding1 =>
+      const AssetGenImage('assets/images/onboarding_1.png');
+
+  /// File path: assets/images/onboarding_2.png
+  AssetGenImage get onboarding2 =>
+      const AssetGenImage('assets/images/onboarding_2.png');
+
+  /// File path: assets/images/onboarding_3.png
+  AssetGenImage get onboarding3 =>
+      const AssetGenImage('assets/images/onboarding_3.png');
+
+  /// File path: assets/images/placeholder_image.jpeg
+  AssetGenImage get placeholderImage =>
+      const AssetGenImage('assets/images/placeholder_image.jpeg');
+
+  /// File path: assets/images/welcome_image.png
+  AssetGenImage get welcomeImage =>
+      const AssetGenImage('assets/images/welcome_image.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        errorImage,
+        homePopular1,
+        homePopular2,
+        homePopular3,
+        homePopular4,
+        onboarding1,
+        onboarding2,
+        onboarding3,
+        placeholderImage,
+        welcomeImage
+      ];
+}
+
+class $AssetsLottieGen {
+  const $AssetsLottieGen();
+
+  /// File path: assets/lottie/Wede_Animation.json
+  String get wedeAnimation => 'assets/lottie/Wede_Animation.json';
+
+  /// File path: assets/lottie/add_to_cart.json
+  String get addToCart => 'assets/lottie/add_to_cart.json';
+
+  /// File path: assets/lottie/bird_ring.lottie
+  String get birdRing => 'assets/lottie/bird_ring.lottie';
+
+  /// File path: assets/lottie/cart_is_empty.json
+  String get cartIsEmpty => 'assets/lottie/cart_is_empty.json';
+
+  /// File path: assets/lottie/empty_screen.json
+  String get emptyScreen => 'assets/lottie/empty_screen.json';
+
+  /// File path: assets/lottie/favourite.json
+  String get favourite => 'assets/lottie/favourite.json';
+
+  /// File path: assets/lottie/hamburger.json
+  String get hamburger => 'assets/lottie/hamburger.json';
+
+  /// File path: assets/lottie/image_shimmer.json
+  String get imageShimmer => 'assets/lottie/image_shimmer.json';
+
+  /// File path: assets/lottie/item_not_found.json
+  String get itemNotFound => 'assets/lottie/item_not_found.json';
+
+  /// File path: assets/lottie/loading.json
+  String get loading => 'assets/lottie/loading.json';
+
+  /// File path: assets/lottie/loading_icecream.json
+  String get loadingIcecream => 'assets/lottie/loading_icecream.json';
+
+  /// File path: assets/lottie/loading_spinner.json
+  String get loadingSpinner => 'assets/lottie/loading_spinner.json';
+
+  /// File path: assets/lottie/loading_spinner_alt.json
+  String get loadingSpinnerAlt => 'assets/lottie/loading_spinner_alt.json';
+
+  /// File path: assets/lottie/lottie_food_loading.json
+  String get lottieFoodLoading => 'assets/lottie/lottie_food_loading.json';
+
+  /// File path: assets/lottie/lottie_list_loading.json
+  String get lottieListLoading => 'assets/lottie/lottie_list_loading.json';
+
+  /// File path: assets/lottie/lottie_round_food_loading.json
+  String get lottieRoundFoodLoading =>
+      'assets/lottie/lottie_round_food_loading.json';
+
+  /// File path: assets/lottie/not_found.json
+  String get notFound => 'assets/lottie/not_found.json';
+
+  /// File path: assets/lottie/rating.json
+  String get rating => 'assets/lottie/rating.json';
+
+  /// File path: assets/lottie/remove_from_cart.json
+  String get removeFromCart => 'assets/lottie/remove_from_cart.json';
+
+  /// File path: assets/lottie/restaurant_loading.json
+  String get restaurantLoading => 'assets/lottie/restaurant_loading.json';
+
+  /// File path: assets/lottie/success.json
+  String get success => 'assets/lottie/success.json';
+
+  /// File path: assets/lottie/success1.json
+  String get success1 => 'assets/lottie/success1.json';
+
+  /// File path: assets/lottie/waiting.json
+  String get waiting => 'assets/lottie/waiting.json';
+
+  /// List of all assets
+  List<String> get values => [
+        wedeAnimation,
+        addToCart,
+        birdRing,
+        cartIsEmpty,
+        emptyScreen,
+        favourite,
+        hamburger,
+        imageShimmer,
+        itemNotFound,
+        loading,
+        loadingIcecream,
+        loadingSpinner,
+        loadingSpinnerAlt,
+        lottieFoodLoading,
+        lottieListLoading,
+        lottieRoundFoodLoading,
+        notFound,
+        rating,
+        removeFromCart,
+        restaurantLoading,
+        success,
+        success1,
+        waiting
+      ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottieGen lottie = $AssetsLottieGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
