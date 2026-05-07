69ee089ce894582e318a16e4d308fdaaee244622
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-05-08 04:23:46 +0600

    Emoji Picker added in chat inbox screen

--- lib/features/contractor/inbox/presentation/widget/composer.dart
+++ lib/features/contractor/inbox/presentation/widget/composer.dart
@@ -5,87 +5,7 @@ import 'package:template_flutter/common_widgets/custom_textform_field.dart';
 // import 'package:template_flutter/constants/text_font_style.dart';
 import 'package:template_flutter/gen/colors.gen.dart';
 import 'package:template_flutter/helpers/ui_helpers.dart';
-
-// class ContractorChatComposer extends StatelessWidget {
-// 	const ContractorChatComposer({
-// 		super.key,
-// 		required this.controller,
-// 		this.onSend,
-// 	});
-
-// 	final TextEditingController controller;
-// 	final VoidCallback? onSend;
-
-// 	@override
-// 	Widget build(BuildContext context) {
-// 		return Container(
-// 			decoration: BoxDecoration(
-// 				color: AppColors.scaffoldColor,
-// 				border: Border(
-// 					top: BorderSide(
-// 						color: AppColors.c000000.withValues(alpha: 0.08),
-// 						width: 1,
-// 					),
-// 				),
-// 			),
-// 			padding: EdgeInsets.fromLTRB(14.w, 14.h, 14.w, 14.h),
-// 			child: SafeArea(
-// 				top: false,
-// 				child: Row(
-// 					children: [
-// 						InkWell(
-// 							borderRadius: BorderRadius.circular(20.r),
-// 							onTap: () {},
-// 							child: Padding(
-// 								padding: EdgeInsets.all(6.w),
-// 								child: Image.asset(
-// 									'assets/icons/attach.png',
-// 									width: 18.w,
-// 									height: 18.h,
-// 								),
-// 							),
-// 						),
-// 						UIHelper.horizontalSpace(8.w),
-// 						InkWell(
-// 							borderRadius: BorderRadius.circular(20.r),
-// 							onTap: () {},
-// 							child: Padding(
-// 								padding: EdgeInsets.all(6.w),
-// 								child: Image.asset(
-// 									'assets/icons/emoji.png',
-// 									width: 18.w,
-// 									height: 18.h,
-// 								),
-// 							),
-// 						),
-// 						UIHelper.horizontalSpace(8.w),
-// 						Expanded(
-// 							child: CustomTextFormField(
-//                 controller: controller,
-//                 hintText: 'Type a message...',
-//                 contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
-//               ),
-// 						),
-// 						UIHelper.horizontalSpace(8.w),
-//             CustomButton(
-//               width: 95.w,
-//               label: 'Send',
-//               onPressed: onSend,
-//               height: 37.h,
-//               padding: EdgeInsets.symmetric(horizontal: 16.w),
-//               color: AppColors.contractor_primary,
-//               leading: Image.asset(
-//                 'assets/icons/send.png',
-//                 width: 18.w,
-//                 height: 18.h,
-//               ),
-//             )
-// 					],
-// 				),
-// 			),
-// 		);
-// 	}
-// }
+import 'package:awesome_emoji_picker/awesome_emoji_picker.dart';
 
 class ContractorChatComposer extends StatefulWidget {
   const ContractorChatComposer({
@@ -107,6 +27,7 @@ class _ContractorChatComposerState extends State<ContractorChatComposer> {
   late FocusNode _focusNode;
   bool isFocused = false;
   bool _showLabel = true;
+  bool showEmojiPicker = false;
 
   @override
   void initState() {
@@ -117,8 +38,9 @@ class _ContractorChatComposerState extends State<ContractorChatComposer> {
     _focusNode.addListener(() {
       final hasFocus = _focusNode.hasFocus;
       setState(() {
-        isFocused = hasFocus;
+        isFocused = _focusNode.hasFocus;
         if (hasFocus) _showLabel = false;
+        if (isFocused) showEmojiPicker = false;
       });
 
       // When losing focus, wait for the width animation to finish before
@@ -144,118 +66,151 @@ class _ContractorChatComposerState extends State<ContractorChatComposer> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.scaffoldColor,
-        border: Border(
-          top: BorderSide(
-            color: AppColors.c000000.withValues(alpha: 0.08),
-            width: 1,
-          ),
-        ),
-      ),
-      padding: EdgeInsets.fromLTRB(
-        14.w,
-        14.h,
-        14.w,
-        14.h,
-      ),
-      child: SafeArea(
-        top: false,
-        child: Row(
-          children: [
-            /// HIDE ICONS WHEN FOCUSED
-            if (!isFocused) ...[
-              InkWell(
-                borderRadius: BorderRadius.circular(20.r),
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.all(6.w),
-                  child: Image.asset(
-                    'assets/icons/attach.png',
-                    width: 20.w,
-                    height: 20.h,
-                  ),
-                ),
-              ),
-
-              UIHelper.horizontalSpace(6.w),
-
-              InkWell(
-                borderRadius: BorderRadius.circular(20.r),
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.all(6.w),
-                  child: Image.asset(
-                    'assets/icons/image.png',
-                    width: 18.w,
-                    height: 18.h,
-                  ),
-                ),
-              ),
-              UIHelper.horizontalSpace(8.w),
-            ] else ...[
-              InkWell(
-                borderRadius: BorderRadius.circular(20.r),
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.only(right: 8.w),
-                  child: Image.asset(
-                    'assets/icons/add.png',
-                    width: 22.w,
-                    height: 22.h,
-                    color: AppColors.c636363.withValues(alpha: 0.6),
-                  ),
-                ),
-              ),
-            ],
-            /// EXPANDING TEXTFIELD
-            Expanded(
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 250),
-                child: CustomTextFormField(
-                  focusNode: _focusNode,
-                  controller: widget.controller,
-                  hintText: 'Type a message...',
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 16.w,
-                    vertical: 14.h,
-                  ),
-                  suffixIcon: Image.asset(
-                    'assets/icons/emoji.png',
-                    width: 8.w,
-                    height: 8.h,
-                    fit: BoxFit.contain,
-                  ),
-                ),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.scaffoldColor,
+            border: Border(
+              top: BorderSide(
+                color: AppColors.c000000.withValues(alpha: 0.08),
+                width: 1,
               ),
             ),
-            UIHelper.horizontalSpace(8.w),
-            /// ANIMATED SEND BUTTON
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 250),
-              curve: Curves.easeInOut,
-              width: isFocused ? 47.w : 95.w,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(14.r),
-                child: CustomButton(
-                  label: _showLabel ? 'Send' : '',
-                  gap: _showLabel,
-                  onPressed: widget.onSend,
-                  height: 37.h,
-                  padding: EdgeInsets.symmetric(horizontal: 12.w),
-                  color: AppColors.contractor_primary,
-                  leading: Image.asset(
-                    'assets/icons/send.png',
-                    width: 18.w,
-                    height: 18.h,
-                  ),
+          ),
+          padding: EdgeInsets.fromLTRB(
+            14.w,
+            14.h,
+            14.w,
+            14.h,
+          ),
+          child: SafeArea(
+            top: false,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    /// HIDE ICONS WHEN FOCUSED
+                    if (!isFocused) ...[
+                      InkWell(
+                        borderRadius: BorderRadius.circular(20.r),
+                        onTap: () {},
+                        child: Padding(
+                          padding: EdgeInsets.all(6.w),
+                          child: Image.asset(
+                            'assets/icons/attach.png',
+                            width: 20.w,
+                            height: 20.h,
+                          ),
+                        ),
+                      ),
+                
+                      UIHelper.horizontalSpace(6.w),
+                
+                      InkWell(
+                        borderRadius: BorderRadius.circular(20.r),
+                        onTap: () {},
+                        child: Padding(
+                          padding: EdgeInsets.all(6.w),
+                          child: Image.asset(
+                            'assets/icons/image.png',
+                            width: 18.w,
+                            height: 18.h,
+                          ),
+                        ),
+                      ),
+                      UIHelper.horizontalSpace(8.w),
+                    ] else ...[
+                      InkWell(
+                        borderRadius: BorderRadius.circular(20.r),
+                        onTap: () {},
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 8.w),
+                          child: Image.asset(
+                            'assets/icons/add.png',
+                            width: 22.w,
+                            height: 22.h,
+                            color: AppColors.c636363.withValues(alpha: 0.6),
+                          ),
+                        ),
+                      ),
+                    ],
+                    /// EXPANDING TEXTFIELD
+                    Expanded(
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 250),
+                        child: CustomTextFormField(
+                          focusNode: _focusNode,
+                          controller: widget.controller,
+                          hintText: 'Type a message...',
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.w,
+                            vertical: 14.h,
+                          ),
+                          suffixIcon: Image.asset(
+                            'assets/icons/emoji.png',
+                            width: 8.w,
+                            height: 8.h,
+                            fit: BoxFit.contain,
+                          ),
+                          onSuffixIconTap: () {
+                            setState(() {
+                              showEmojiPicker = !showEmojiPicker;
+                              FocusScope.of(context).unfocus();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    UIHelper.horizontalSpace(8.w),
+                    /// ANIMATED SEND BUTTON
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 250),
+                      curve: Curves.easeInOut,
+                      width: isFocused ? 47.w : 95.w,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(14.r),
+                        child: CustomButton(
+                          label: _showLabel ? 'Send' : '',
+                          gap: _showLabel,
+                          onPressed: widget.onSend,
+                          height: 37.h,
+                          padding: EdgeInsets.symmetric(horizontal: 12.w),
+                          color: AppColors.contractor_primary,
+                          leading: Image.asset(
+                            'assets/icons/send.png',
+                            width: 18.w,
+                            height: 18.h,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-            )
-          ],
+              ],
+            ),
+          ),
         ),
-      ),
+        /// EMOJI PICKER
+        // if (showEmojiPicker)
+        AnimatedSize(
+          duration: Duration(milliseconds: 200),
+          child: showEmojiPicker
+          ? SizedBox(
+            height: 280.h,
+            child: AwesomeEmojiPicker(
+              categoryIconColor: AppColors.c000000.withValues(alpha: 0.45),
+              searchHintText: 'Search emojis',
+              onEmojiSelected: (emoji) {
+                widget.controller.text += emoji.char;
+                widget.controller.selection = TextSelection.fromPosition(
+                  TextPosition(offset: widget.controller.text.length),
+                );
+              },
+            ),
+          ) : SizedBox.shrink(),
+        ),
+      ],
     );
   }
 }
