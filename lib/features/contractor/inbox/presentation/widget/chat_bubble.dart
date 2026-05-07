c1ce7a9075289b5376e7b1e3f43e4f266587f35c
Kazi Asif Raihan E.Haque <157726357+KaziAsif406@users.noreply.github.com>
2026-05-08 00:19:29 +0600

    Chat Bubbles inside Chat Inbox screen updated

--- lib/features/contractor/inbox/presentation/widget/chat_bubble.dart
+++ lib/features/contractor/inbox/presentation/widget/chat_bubble.dart
@@ -16,63 +16,100 @@ class ContractorChatMessage {
 	final bool isMe;
 }
 
-class ContractorChatBubble extends StatelessWidget {
+class ContractorChatBubble extends StatefulWidget {
 	const ContractorChatBubble({super.key, required this.message});
 
 	final ContractorChatMessage message;
 
+  @override
+  State<ContractorChatBubble> createState() => _ContractorChatBubbleState();
+}
+
+class _ContractorChatBubbleState extends State<ContractorChatBubble> {
+  bool showTime = false;
+
 	@override
 	Widget build(BuildContext context) {
-		final isMe = message.isMe;
+		final isMe = widget.message.isMe;
 
-		return Align(
-			alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-			child: Container(
-				width: 0.82.sw,
-				margin: EdgeInsets.only(bottom: 14.h),
-				padding: EdgeInsets.fromLTRB(16.w, 14.h, 16.w, 12.h),
-				decoration: BoxDecoration(
-					color: isMe ? AppColors.contractor_primary : AppColors.scaffoldColor,
-					borderRadius: isMe? BorderRadius.only(
-						topLeft: Radius.circular(18.r),
-						topRight: Radius.circular(18.r),
-						bottomLeft: Radius.circular(18.r),
-						bottomRight: Radius.circular(0.r) 
-            ) 
-            : BorderRadius.only(
-            topLeft: Radius.circular(18.r),
-            topRight: Radius.circular(18.r),
-            bottomLeft: Radius.circular(0.r),
-            bottomRight: Radius.circular(18.r),
-					),
-				),
-				child: Column(
-					crossAxisAlignment:
-							isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-					children: [
-						Text(
-							message.text,
-							style: (isMe
-											? TextFontStyle.textStyle18cFFFFFFInter400
-											: TextFontStyle.textStyle18c14181FInter600)
-									.copyWith(
-								fontSize: 14.sp,
-								fontWeight: FontWeight.w500,
-								height: 1.4,
-							),
-						),
-						UIHelper.verticalSpace(8.h),
-						Text(
-							message.time,
-							style: (isMe
-											? TextFontStyle.textStyle16cFFFFFFInter500
-											: TextFontStyle.textStyle16c64748BInter400)
-									.copyWith(fontSize: 11.sp),
-						),
-					],
-				),
-			),
-		);
+		return GestureDetector(
+      onTap: () {
+        setState(() {
+          showTime = !showTime;
+        });
+      },
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+
+          /// TIME
+          if (showTime)
+            Padding(
+              padding: EdgeInsets.only(bottom: 6.h),
+              child: Center(
+                child: Text(
+                  widget.message.time,
+                  style: TextStyle(
+                    fontSize: 11.sp,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+
+          /// BUBBLE
+          Align(
+            alignment:
+                isMe ? Alignment.centerRight : Alignment.centerLeft,
+            child: Container(
+              // width: 0.82.sw,
+              margin: isMe
+                  ? EdgeInsets.only(bottom: 14.h, left: 50.w)
+                  : EdgeInsets.only(bottom: 14.h, right: 50.w),
+              // EdgeInsets.only(bottom: 14.h),
+              padding: EdgeInsets.fromLTRB(
+                20.w,
+                14.h,
+                20.w,
+                12.h,
+              ),
+              decoration: BoxDecoration(
+                color: isMe
+                    ? AppColors.contractor_primary
+                    : AppColors.scaffoldColor,
+                borderRadius: isMe
+                    ? BorderRadius.only(
+                        topLeft: Radius.circular(18.r),
+                        topRight: Radius.circular(18.r),
+                        bottomLeft: Radius.circular(18.r),
+                        bottomRight: Radius.circular(0.r),
+                      )
+                    : BorderRadius.only(
+                        topLeft: Radius.circular(18.r),
+                        topRight: Radius.circular(18.r),
+                        bottomLeft: Radius.circular(0.r),
+                        bottomRight: Radius.circular(18.r),
+                      ),
+              ),
+              child: Text(
+                widget.message.text,
+                style: (isMe
+                        ? TextFontStyle
+                            .textStyle18cFFFFFFInter400
+                        : TextFontStyle
+                            .textStyle18c14181FInter600)
+                    .copyWith(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  height: 1.4,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
 	}
 }
 
